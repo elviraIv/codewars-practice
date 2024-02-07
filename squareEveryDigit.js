38092ea411d7b3e35a73d2d38787e7db6255fb9b
Elvira <elvira.n.ivanova@gmail.com>
2024-02-07 19:56:15 +0200

    Add solution

---
+++ squareEveryDigit.js
@@ -0,0 +1,19 @@
+// You are asked to square every digit of a number and concatenate them.
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+function squareDigits(num) {
+  let stringArray = num.toString().split("");
+
+  const digitsArray = stringArray.reduce((acc, curr) => {
+    acc.push(parseInt(curr));
+    return acc;
+  }, []);
+
+  const resultArray = [];
+
+  for (let el of digitsArray) {
+    resultArray.push(el * el);
+  }
+
+  return Number(resultArray.join(""));
+}
